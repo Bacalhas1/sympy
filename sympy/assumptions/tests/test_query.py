6294bc4441698b3329f010217e03894ef767bfc2
Martim Barreiros <your.email@tecnico.ulisboa.pt>
2025-03-27 00:50:02 +0000

    Fix #27726: Should not finite imply infinite?
    This #27726 fix ensures that:
    x = symbols("x")
    -ask(Q.infinite(x), ~Q.finite(x)) # printed None before
    the fix and returns True after
    -Removed an unnecessary predicate that implied a
    commutative object must be
    either finite or infinite, which conflicted with the new rule.

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -2521,3 +2521,7 @@ def test_issue_25221():
     assert ask(Q.transcendental(x), Q.algebraic(x) | Q.positive(y,y)) is None
     assert ask(Q.transcendental(x), Q.algebraic(x) | (0 > y)) is None
     assert ask(Q.transcendental(x), Q.algebraic(x) | Q.gt(0,y)) is None
+
+def test_issue_27726():
+    x = symbols("x")
+    assert ask(Q.infinite(x), ~Q.finite(x)) is True
